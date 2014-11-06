0565d2eb1a1615dd307e8bc9fa12fba6cee3f6f6
epitsin <epitsin@yahoo.com>
2014-11-06 17:02:48 +0200

    Added Data and Models project and moved the dbContext and the ApplicationUser there

---
+++ Foodsy.Data/FoodsyDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Foodsy.Data
+{
+    using Foodsy.Data.Models;
+
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class FoodsyDbContext : IdentityDbContext<User>
+    {
+        public FoodsyDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static FoodsyDbContext Create()
+        {
+            return new FoodsyDbContext();
+        }
+    }
+}
--- Foodsy.Web/Controllers/AccountController.cs
+++ Foodsy.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Foodsy.Web.Models;
+using Foodsy.Data.Models;
 
 namespace Foodsy.Web.Controllers
 {
@@ -154,7 +155,7 @@ namespace Foodsy.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -370,7 +371,7 @@ namespace Foodsy.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {

9e4f69ecd75b0bf26e6bf5ca354a5539b7455fcf
epitsin <epitsin@yahoo.com>
2014-11-07 13:56:38 +0200

    Select Razor as the only engine

---
+++ Foodsy.Web/App_Start/ViewEnginesConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Foodsy.Web.App_Start
+{
+    public class ViewEnginesConfig
+    {
+        public static void RegisterViewEngines(ViewEngineCollection viewEngineCollection)
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+        }
+    }
+}

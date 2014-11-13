968ac2c99765b070341a87b4243627046cca4255
epitsin <epitsin@yahoo.com>
2014-11-13 15:53:27 +0200

    Nicer view for errors.

--- Foodsy.Web/Areas/Shop/ViewModels/RecipeViewModel.cs
+++ Foodsy.Web/Areas/Shop/ViewModels/RecipeViewModel.cs
@@ -1,15 +1,24 @@
 ﻿namespace Foodsy.Web.Areas.Shop.ViewModels
 {
+    using AutoMapper;
+
     using Foodsy.Data.Models;
     using Foodsy.Web.Areas.Administration.Models.Base;
     using Foodsy.Web.Infrastructure.Mapping;
 
-    public class RecipeViewModel : AdministrationViewModel, IMapFrom<RecipeShoppingCart>
+    public class RecipeViewModel : AdministrationViewModel, IMapFrom<RecipeShoppingCart>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
         public int Portions { get; set; }
 
-        public Recipe Recipe { get; set; }
+        public string Name { get; set; }
+
+        public decimal PricePerPortion { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            //throw new System.NotImplementedException();
+        }
     }
 }
